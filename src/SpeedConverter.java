903fad3543c42eeedc78ce4ec27928ce98a64f69
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-04 19:02:41 -0300

    Testa aplicacao

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,10 +1,11 @@
 public class SpeedConverter {
     public static void main(String[] args) {
-
+       long milesPerHour = toMilesPerHour(280);
+        System.out.println(milesPerHour);
 
     }
     public static long toMilesPerHour(double kilometersPerHour) {
-        long longKilometer = (long)Math.round(kilometersPerHour * 0.621371);
-        return longKilometer;
+        // converte o double em long e arredonda o valor para cima
+        return (long)Math.round(kilometersPerHour * 0.621371);
     }
 }

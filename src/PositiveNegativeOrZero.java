66ba7c3c46bb36869f72eb275765c1fad31da22c
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-04 18:29:51 -0300

    Cria metodo checkNumber ,cria a estrutura de controle

--- src/PositiveNegativeOrZero.java
+++ src/PositiveNegativeOrZero.java
@@ -1,2 +1,15 @@
 public class PositiveNegativeOrZero {
+    public static void main(String[] args) {
+
+        checkNumber(1);
+    }
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0 ) {
+            System.out.println("Negative");
+        }else {
+            System.out.println("Zero");
+        }
+    }
 }

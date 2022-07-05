7dbed132fbbf7d380a6127844406fb9287287de5
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-05 17:31:50 -0300

    Cria projeto e o metodo

---
+++ src/DecimalComparator.java
@@ -0,0 +1,8 @@
+public class DecimalComparator {
+    public static void main(String[] args) {
+
+    }
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
+
+    }
+}

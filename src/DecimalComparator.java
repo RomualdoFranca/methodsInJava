5b8a16bd07ef96feb741670c31fd6ec4cf34c96a
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-05 18:15:35 -0300

    Tentativa de criar a logica para comparar os numeros

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,8 +1,18 @@
 public class DecimalComparator {
     public static void main(String[] args) {
-
+        boolean equalNum = areEqualByThreeDecimalPlaces(-3.1756, -3.1757);
+        System.out.println(equalNum);
     }
     public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
 
+        if (number1 >= number2) {
+
+            if (number1 - number2 <= 0.0009 || number1 - number2 <= -0.0009) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
 }

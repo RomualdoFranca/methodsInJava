8362b234b7048566d89019810f5579634b577df8
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-05 18:28:29 -0300

    Cria a logica de comparacao entre os dois numeros e acrescenta commetario do reciocinio utilizado

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,18 +1,26 @@
 public class DecimalComparator {
     public static void main(String[] args) {
-        boolean equalNum = areEqualByThreeDecimalPlaces(-3.1756, -3.1757);
+        boolean equalNum = areEqualByThreeDecimalPlaces(3.1756, 3.1757);
         System.out.println(equalNum);
     }
     public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
-
+        //  Foi necessario criar duas possibilidadas pois nao ha como prever que o 1° n digitado sempre sera o maior
         if (number1 >= number2) {
-
-            if (number1 - number2 <= 0.0009 || number1 - number2 <= -0.0009) {
+            //  A forma encontrada de comparar dois numeros ate a terceira casa decimal foi atraves da subtracao
+            //  O desafio exige que os numeros sejam iguais ate a 3° casa decimal
+            //  A diferença entre dois n iguais ate a 3° casa decimal sempre da como resultado 0.000
+            //  O numero da 4° casa em diante nao faz diferença
+            if (number1 - number2 <= 0.0009) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (number2 - number1 <= 0.0009) {
                 return true;
             } else {
                 return false;
             }
         }
-        return false;
     }
 }
